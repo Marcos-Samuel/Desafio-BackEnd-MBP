5dd3d2613c94cf45ac5114c2cf43b83aa0192551
Marcos-Samuel <marcos_samueloff@outlook.com>
2023-12-31 09:58:40 -0300

    <FIX>[organizando arquivos]: <deploy>

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT , () => {
     console.log(`Server listening on port ${process.env.PORT || 3000}`);
 });
