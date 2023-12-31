69c25a27f68e276ba0815dadf7efec7939a88fdc
Marcos-Samuel <marcos_samueloff@outlook.com>
2023-12-31 10:58:20 -0300

    <FIX>[organizando arquivos]: <deploy- add cors>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
+
 
 app.listen(process.env.PORT , () => {
     console.log(`Server listening on port ${process.env.PORT}`);
